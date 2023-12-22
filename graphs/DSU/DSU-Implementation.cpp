da2787f8c48e7cfe4f6175f81648109b476a583a
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2023-12-22 22:34:42 +0530

    Update DSU-Implementation.cpp

---
+++ graphs/DSU/DSU-Implementation.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Disjoint{
+
+    vector<int> parent;
+    vector<int> rank;
+    int n;
+
+public:
+    Disjoint(int n){
+        parent.resize(n);
+        rank.resize(n,0);
+        this->n=n;
+        makeSet();
+    }
+
+    void makeSet(){
+        for(int i=0; i<n; i++){
+            parent[i]=i;      
+        }
+    }
+
+    int find(int i){
+        if(parent[i]==i) return i;
+        return parent[i]=find(parent[i]);
+    }
+
+    void union_set(int a, int b){
+
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            if(rank[a]<rank[b]) parent[a]=b;
+            else if(rank[a]>rank[b])parent[b]=a;
+            else{
+                parent[b]=a;
+                rank[a]++;
+            } 
+        }
+        return;
+    }
+
+
+};
+
